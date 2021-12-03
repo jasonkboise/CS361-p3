591e8bde4fcb7670c734e98f338f3b72e0dd0285
jasonkboise <54860002+jasonkboise@users.noreply.github.com>
2021-12-02 20:41:37 -0700

    continuing work

--- re/RE.java
+++ re/RE.java
@@ -24,7 +24,7 @@ public class RE implements REInterface{
             NFA regex = regex();
 
             NFA combined = new NFA();
-            String name = "q" + count++;
+            String name = Integer.toString(count++);
             combined.addStartState(name);
 
             combined.addNFAState(regex.getStates());
@@ -59,7 +59,16 @@ public class RE implements REInterface{
     }
 
     private NFA concat(NFA n1, NFA n2) {
-        String n2Start = n2.getStartState().getName();
+        Set<NFAState> n1Finals = n1.getFinalStates();
+
+        n1.addNFAStates(n2.getStates());
+
+        for (NFAState state: n1Finals) {
+            state.setNonFinal();
+            n1.addTransition(state.getName(), 'e', n2.getStartState().getName());
+        }
+
+        n1.addAbc(n2.getABC());
     }
 
     private char peek() {
@@ -80,13 +89,33 @@ public class RE implements REInterface{
         eat(c);
         return c;
     }
-    private NFA root() {
     
+    private NFA newNFA(char n) {
+        NFA newNFA = new NFA();
+        
+        String start = Integer.toString(count++);
+        nfa.addStartState(start);
+        
+        String final = Integer.toString(count++);
+        nfa.addFinalState(final);
+
+        nfa.addTransitions(start, n, final);
+
+        Set<Character> abc;
+        abc = new LinkedHashSet<Character>();
+        abc.add(n);
+        nfa.addAbc(abc);
+        
+        return nfa;
+
+    }
+
+    private NFA root() {
         if (peek() == '(') {
-        eat('(');
-        NFA reg1 = regEx();
-        eat(')');
-        return reg1;
+            eat('(');
+            NFA reg1 = regEx();
+            eat(')');
+            return reg1;
         }
         
         return newNFA(next());
